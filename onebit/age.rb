8d9871a6b4ca3daaebf4c0b6d481ccc37375ace2
Larissa Lani Soares Macedo <larissa.macedo@dbserver.com.br>
2019-01-31 13:56:40 -0200

    update

--- onebit/age.rb
+++
@@ -1,7 +0,0 @@
-print 'Insira seu nome: '
-name = gets.chomp
-
-print 'Insira sua idade: '
-age = gets.chomp.to_i
-
-puts " Me chamo #{name} e tenho #{age} anos."
--- onebit/each_array.rb
+++
@@ -1,8 +0,0 @@
-
-names = ['Maria', 'João', 'Teresa']
-
-name = 'Leonardo Sousa'
-
-names.each do |name|
-    puts name + ' é o meu nome'
-end
